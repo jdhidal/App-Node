9edd35e47cd5714eb07e91ae4fb4e5514667caab
jdhidal <diegohidalgo673@gmail.com>
2024-11-15 20:00:55 -0500

    test: :rocket: Validation test in new action od deploy in test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 //API URL
 app.get('/', (req, res) => {
-  res.send('¡Hello Word José Diego Hidalgo Lucas!');
+  res.send('¡Hello Word José Diego Hidalgo Lucas version 1!');
 });
 
 app.listen(PORT, () => {
